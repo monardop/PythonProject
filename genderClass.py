518d9e311d3efadc002c422c970ea29fc56f9379
Pablo <pablomonardo@gmail.com>
2023-02-07 10:17:38 -0300

    Classify people according to data

---
+++ genderClass.py
@@ -0,0 +1,20 @@
+from sklearn import tree
+
+# [heigh, weight, shoes size]
+X = [
+    [181, 80, 44], [177, 70, 43], [160, 60, 38],
+    [166, 65, 40], [190, 90,47], [175, 64, 39],
+    [177, 70, 40], [171, 75, 42], [181, 85, 43] 
+]
+Y = [
+    'male', 'female', 'female', 
+    'male', 'male', 'female',
+    'female', 'male', 'male'
+]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,Y)
+
+prediction = clf.predict([[155, 50, 36]])
+
+print(prediction)
