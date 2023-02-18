63b76e1b698b68547258328d2b12aca25f5495f9
Pablo <pablomonardo@gmail.com>
2023-02-18 17:31:08 -0300

    Update main.py

--- HackerRank/minMaxSum/main.py
+++ HackerRank/minMaxSum/main.py
@@ -14,7 +14,7 @@ def miniMaxSum(arr):
         if total_sum > max_sum:
             max_sum = total_sum
 
-    min_sum = max_sum    
+    min_sum = max_sum
     for i in range(size_arr):
         total_sum = 0
         for x in range(size_arr):
@@ -24,8 +24,5 @@ def miniMaxSum(arr):
                 total_sum += arr[x]
         if total_sum < min_sum:
             min_sum = total_sum
-    
-    print(f"{min_sum} {max_sum}")
-        
 
-miniMaxSum([1,2,3,4,5])
+    return f"{min_sum} {max_sum}"
