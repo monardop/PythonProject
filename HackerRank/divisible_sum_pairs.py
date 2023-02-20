338d0bd84de213dd8f87f39d75949a8a2107aa76
Pablo <pablomonardo@gmail.com>
2023-02-20 15:22:20 -0300

    Create divisible_sum_pairs.py

---
+++ HackerRank/divisible_sum_pairs.py
@@ -0,0 +1,23 @@
+def divisibleSumPairs(n: int, k: int, ar: list) -> int:
+    """
+    Given an array of integers and positive integer k, 
+    determine the number of (i,j) pairs where 
+    i<j and ar[i] + ar[j] is divisible by k
+
+    :param n:length of array
+    :param k: the integer divisor
+    :param ar:array of int
+    :return: number of pairs
+    """
+    count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            ar[i]
+            ar[j]
+            suma = ar[i] + ar[j]
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+    return count
+
+
+print(divisibleSumPairs(6, 3, [1,3,2,6,1,2]))
