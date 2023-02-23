d425729adb70923a3cac77f62a8eaddc10f97a7e
Pablo <pablomonardo@gmail.com>
2023-02-23 16:07:44 -0300

    Create review_strings.py

---
+++ HackerRank/30DaysChallenge/review_strings.py
@@ -0,0 +1,8 @@
+def string_review(phrase: str) -> None:
+    even = ""
+    odd = ""
+    for n, letter in enumerate(phrase):
+        print(n)
+    return f"{even} {odd}"
+
+string_review("Hacker")
