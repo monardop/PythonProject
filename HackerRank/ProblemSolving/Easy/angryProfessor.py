3312fd6a78b7ca54d95d84f6e0c845abb466cab7
Pablo <pablomonardo@gmail.com>
2023-02-26 16:17:04 -0300

    Create angryProfessor.py

---
+++ HackerRank/ProblemSolving/Easy/angryProfessor.py
@@ -0,0 +1,11 @@
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+def angryProfessor(k: int, a: list[int]) -> str:
+    pass
+    
+
