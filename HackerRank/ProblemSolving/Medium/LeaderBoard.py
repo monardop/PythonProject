9d8ea5c7d4724cfe2bb7b8ee9072d3870e525245
Pablo <pablomonardo@gmail.com>
2023-02-26 11:05:07 -0300

    Update LeaderBoard.py

--- HackerRank/ProblemSolving/Medium/LeaderBoard.py
+++ HackerRank/ProblemSolving/Medium/LeaderBoard.py
@@ -13,5 +13,31 @@ def climbingLeaderboard(ranked: list[int], player: list[int]) -> list[int]:
             positions.append(len(ranked ) + 1)
     return positions
 
-print(climbingLeaderboard([100,90,90,80], [70,80,105]))
 
+
+def climbingLeaderboard_alt(ranked, player):
+    # Write your code here
+    stack = []
+    for i,r in enumerate(ranked):
+        if i > 0 and ranked[i] == ranked[i-1]:
+            continue
+        stack.append(ranked[i])
+        
+    position = {}
+    for i,p in enumerate(player):
+        if p in position:
+            position[p].append(i)
+        else:
+            position[p] = [i]
+            
+    newplayer = sorted(player)
+    ranks = {}
+    for i,p in enumerate(newplayer):
+        while stack and stack[-1] <= p:
+            stack.pop()
+        if p not in ranks:
+            ranks[p] = len(stack)+1
+           
+    return [ranks[p] for p in player]
+
+print(climbingLeaderboard_alt([100,90,90,80], [70,80,105]))
