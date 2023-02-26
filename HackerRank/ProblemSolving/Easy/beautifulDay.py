c563c9743dd8907fc55ab927725874c7a9fa6ca9
Pablo <pablomonardo@gmail.com>
2023-02-26 16:39:32 -0300

    Create beautifulDay.py

---
+++ HackerRank/ProblemSolving/Easy/beautifulDay.py
@@ -0,0 +1,13 @@
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    pass
