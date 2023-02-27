fb1bec9e66d585612bc567743c0f15a776c87b7f
Pablo <pablomonardo@gmail.com>
2023-02-27 17:10:38 -0300

    Testeado y terminado

--- HackerRank/30DaysChallenge/D10binary.py
+++ HackerRank/30DaysChallenge/D10binary.py
@@ -13,4 +13,17 @@ def to_binary(number: int) -> str:
     return bin_number[::-1]
 
 def max_num_consecurive(number: int) -> int:
-    pass
+    bin_number: str = to_binary(number)
+    group = []
+    count = 0
+    for pos, bit in enumerate(bin_number):
+        if bit == '1':
+            count += 1
+        if bit == '0' and count > 0:
+            group.append(count)
+            count = 0
+            if len(bin_number) - pos < max(group):
+                break  # To prevent unnecessary cycles
+    return max(group)
+
+print(max_num_consecurive(200))
