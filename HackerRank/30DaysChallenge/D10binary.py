8c8dfe108de6808b541391feb3a8d55d63b76aed
Pablo <pablomonardo@gmail.com>
2023-02-27 17:02:42 -0300

    Transforma a binario correctamente

--- HackerRank/30DaysChallenge/D10binary.py
+++ HackerRank/30DaysChallenge/D10binary.py
@@ -5,7 +5,12 @@ Then find and print the base- integer denoting the maximum number of
 consecutive 's in 's binary representation.
 """
 def to_binary(number: int) -> str:
-    pass
+    bin_number = ""
+    while number//2 != 0:
+        bin_number += str(number%2)
+        number //= 2
+    bin_number += "1"
+    return bin_number[::-1]
 
 def max_num_consecurive(number: int) -> int:
     pass
