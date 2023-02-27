492957f6c3e2184d07ac01fa37ce63aa2e4599ad
Pablo <pablomonardo@gmail.com>
2023-02-27 16:56:05 -0300

    Update D10binary.py

---
+++ HackerRank/30DaysChallenge/D10binary.py
@@ -0,0 +1,11 @@
+"""
+Task
+Given a base-10 integer, , convert it to binary (base-2). 
+Then find and print the base- integer denoting the maximum number of 
+consecutive 's in 's binary representation.
+"""
+def to_binary(number: int) -> str:
+    pass
+
+def max_num_consecurive(number: int) -> int:
+    pass
