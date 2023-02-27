b065f2ff1dd134e9f042b5ecb15ae5a5a543739a
Pablo <pablomonardo@gmail.com>
2023-02-27 17:39:42 -0300

    Update standarDesviation.py

--- HackerRank/Statistic/standarDesviation.py
+++ HackerRank/Statistic/standarDesviation.py
@@ -5,4 +5,6 @@ def stdDev(arr: list[int]) -> float:
     # Print your answers to 1 decimal place within this function
     o = average(arr)
     values = list(map(lambda x: (x - o)**2, arr))
-    return round(sum(values)/len(arr),1)
+    return round((sum(values) / len(arr)) ** (1/2), 1)
+
+print(stdDev([2,5,2,7,4]))
