79c1fcb9b3d919e3b9a8170d9652beba858f05c1
Pablo <pablomonardo@gmail.com>
2023-02-28 18:33:00 -0300

    Create basicProbability.py

---
+++ HackerRank/Statistic/basicProbability.py
@@ -0,0 +1,19 @@
+"""
+Cual es la probabilidad de que, tirando dos dados, 
+la suma de sus caras sea menor o igual que 9
+"""
+
+dice1 = list(range(1,7))
+dice2 = list(range(1,7))
+
+posibilidades = []
+for x in dice1:
+    for y in dice2:
+        posibilidades.append(x+y)
+
+print(
+    "De",len(posibilidades), "posibilidades,", 
+    len([n for n in posibilidades if n <= 9]), "cumplen")
+
+p =  len([n for n in posibilidades if n <= 9]) / len(posibilidades) 
+print(p)
