6ffe4e918a2d660e9ce7aebc4c6632974a76f149
Pablo <pablomonardo@gmail.com>
2023-03-01 11:20:13 -0300

    Update basicProbability.py

--- HackerRank/Statistic/basicProbability.py
+++ HackerRank/Statistic/basicProbability.py
@@ -3,17 +3,37 @@ Cual es la probabilidad de que, tirando dos dados,
 la suma de sus caras sea menor o igual que 9
 """
 
-dice1 = list(range(1,7))
-dice2 = list(range(1,7))
+def ej1():
+    dice1 = list(range(1,7))
+    dice2 = list(range(1,7))
 
-posibilidades = []
-for x in dice1:
-    for y in dice2:
-        posibilidades.append(x+y)
+    posibilidades = []
+    for x in dice1:
+        for y in dice2:
+            posibilidades.append(x+y)
 
-print(
-    "De",len(posibilidades), "posibilidades,", 
-    len([n for n in posibilidades if n <= 9]), "cumplen")
+    print(
+        "De",len(posibilidades), "posibilidades,", 
+        len([n for n in posibilidades if n <= 9]), "cumplen")
 
-p =  len([n for n in posibilidades if n <= 9]) / len(posibilidades) 
-print(p)
+    p =  len([n for n in posibilidades if n <= 9]) / len(posibilidades) 
+    print(p)
+
+
+def ej2():
+    dice1 = list(range(1,7))
+    dice2 = list(range(1,7))
+
+    posibilidades = []
+    for x in dice1:
+        for y in dice2:
+            posibilidades.append(x+y)
+
+    print(
+        "De",len(posibilidades), "posibilidades,", 
+        len([n for n in posibilidades if n == 6 ]), "cumplen")
+
+    p =  len([n for n in posibilidades if n == 6]) / len(posibilidades) 
+    print(p)
+
+ej2()
