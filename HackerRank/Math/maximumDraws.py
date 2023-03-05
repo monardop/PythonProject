185d66231174f995cbc02e020c7d02cac1d6a4e9
Pablo <pablomonardo@gmail.com>
2023-03-05 11:30:28 -0300

    Create maximumDraws.py

---
+++ HackerRank/Math/maximumDraws.py
@@ -0,0 +1,25 @@
+import os
+#
+# Complete the 'maximumDraws' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def maximumDraws(n):
+    # Write your code here
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = maximumDraws(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
