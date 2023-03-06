1d6c491c6d8352faf71597f0448bcec6fb78d974
Pablo <pablomonardo@gmail.com>
2023-03-05 21:19:33 -0300

    Update maximumDraws.py

--- HackerRank/Math/maximumDraws.py
+++ HackerRank/Math/maximumDraws.py
@@ -6,9 +6,11 @@ import os
 # The function accepts INTEGER n as parameter.
 #
 
+
 def maximumDraws(n):
     # Write your code here
-    pass
+    return n + 1
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
